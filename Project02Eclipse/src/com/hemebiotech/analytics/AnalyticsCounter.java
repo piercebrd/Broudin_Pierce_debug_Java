be51e5e1a69874638eda0d96db56f02bdf437445
Pierce Broudin <piercebroudin.pb@gmail.com>
2024-12-20 14:34:23 +0100

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,9 +3,47 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
+
+	private ISymptomReader reader;
+	private ISymptomWriter writer;
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		this.reader = reader;
+		this.writer = writer;
+	}
+
+	// getters and setters ? //
+
+	public List<String> GetSymptoms() {
+		return reader.GetSymptoms();
+	}
+
+	public Map<String, Integer> CountSymptoms(List<String> symptoms) {
+		Map<String, Integer> result = new HashMap<String, Integer>();
+		for (String symptom : symptoms) {
+			result.put(symptom, result.getOrDefault(symptom, 0) + 1);
+		}
+		return result;
+	}
+
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+		// Use a TreeMap to automatically sort by keys
+		return new TreeMap<>(symptoms);
+	}
+
+	public void WriteSymptoms(Map<String, Integer> symptoms) {
+		writer.WriteSymptoms(symptoms);
+	}
+
+
+
+	/* private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 	
@@ -39,5 +77,5 @@ public class AnalyticsCounter {
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
-	}
+	} */
 }
